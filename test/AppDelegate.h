204947b4d39f545a2c5475a4bfd81a9b8ca59256
李国强 <liguoqiang@liguoqiangdeMacBook-Pro.local>
2015-04-16 16:43:47 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 李国强 on 15/4/16.
+//  Copyright (c) 2015年 李国强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 李国强 on 15/4/16.
+//  Copyright (c) 2015年 李国强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
